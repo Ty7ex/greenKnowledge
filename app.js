2370f7729b293ec007648acbdb894d5aad61ab0b
Ty7ex <m.atthias@outlook.jp>
2017-05-20 10:37:18 +0200

    gitignore default

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ var express = require('express');
 
 var app = express();
 
-app.configure(function() {
-  app.set('view engine', 'jade');
-  app.set('views', __dirname + '/views');
-  app.use(express.static(__dirname + '/public'));
-});
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(req, res) {
-  res.render("root.jade");
+  //res.render("root.jade");
+  //var fn = pug.compile('string of pug');
+  res.render('index.html');
 });
 
 app.get('/hallo', function(req, res) {
